46629993818545110585dac558555f599a0041ea
carlosespinozablunda <55984876+carlosespinozablunda@users.noreply.github.com>
2020-01-30 07:42:20 -0600

    Update User.cs

--- User.cs
+++ User.cs
@@ -1,50 +1,50 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace greenBooks
 {
+    
     public class User
     {
-        public enum bankName
-        {
-            Chase = 1,
-            BankOfAmerica = 2,
-            WellsFargo = 3,
-            RoseBank = 4,
-            ProsperityBank = 5,
-            Allegiance = 6,
-            Capital = 7,
-            AllyBank = 8,
-            OneMainFinancial = 9,
-            CitiBank = 10,
-            AIG = 11,
-            GoldmanSachs = 12
+        private Banks bank;
+        private AccountType account;
+        private string firstName;
+        private string lastName;
+        private string emailAddress;
+        public User(string firstName, string lastName, string emailAddress){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.emailAddress = emailAddress;
+            
         }
 
-        public enum accountType
-        {
-            basicChackingAccount=1,
-            savingsAccounts=2,
-            interestBearingCheckingAccount=3,
-            moneyMarketAccount=4,
-            CD=5,
-            IRA=6,
-            brokerageAccount=7
+        public string getFN(){
+            return firstName;
         }
-
-        String fName { get; set;}
-        String lName { get; set;}
-        bankName bn { get; set; }
-        accountType acc { get; set; }
-        Bank 
-
-        public User(String fName, String lName, int bn, int acc)
-        {
-            this.fName = fName;
-            this.lName = lName;
-            this.bn = (bankName)this.bn;
-            this.acc = (accountType)acc;
+        public string getLN(){
+            return lastName;
+        }
+        public string getEmail(){
+            return emailAddress;
+        }
+        public void setFN(string FN){
+            this.firstName = FN;
+        }
+        public void setLN(string LN){
+            this.lastName = LN;
+        }
+        public void setEmail(string email){
+            this.emailAddress = email;
         }
     }
 }
+
+
+
+
+
+   
+        
+      
+   
