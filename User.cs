b33dc246451780f96a54e1b01c986bc2ada4f2b2
Joseph-Garzon <55984853+Joseph-Garzon@users.noreply.github.com>
2020-01-31 07:33:08 -0600

    Update User.cs

--- User.cs
+++ User.cs
@@ -4,47 +4,52 @@ using System.Text;
 
 namespace greenBooks
 {
-    
     public class User
     {
-        private Banks bank;
-        private AccountType account;
-        private string firstName;
-        private string lastName;
-        private string emailAddress;
-        public User(string firstName, string lastName, string emailAddress){
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.emailAddress = emailAddress;
-            
+        public enum bankName
+        {
+            BankOfAmerica = 1,
+            Chase = 2,
+            WellsFargo = 3,
+            RoseBank = 4,
+            ProsperityBank = 5,
+            Allegiance = 6,
+            Capital = 7,
+            AllyBank = 8,
+            OneMainFinancial = 9,
+            CitiBank = 10,
+            AIG = 11,
+            GoldmanSachs = 12
         }
 
-        public string getFN(){
-            return firstName;
+        public enum accountType
+        {
+            basicChackingAccount=1,
+            savingsAccounts=2,
+            interestBearingCheckingAccount=3,
+            moneyMarketAccount=4,
+            CD=5,
+            IRA=6,
+            brokerageAccount=7
         }
-        public string getLN(){
-            return lastName;
+
+        String fName { get; set;}
+        String lName { get; set;}
+        bankName bn { get; set; }
+        accountType acc { get; set; }
+        Bank _bank { get; set; }
+        int status=0;
+        int status {get; set;}
+
+        public User(String fName, String lName, int bn, int acc, int principle, int status)
+        {
+            this.fName = fName;
+            this.lName = lName;
+            this.bn = (bankName)bn;
+            this.acc = (accountType)acc;
+            _bank = new Bank(principle,bn);
+            this.status = status;
         }
-        public string getEmail(){
-            return emailAddress;
-        }
-        public void setFN(string FN){
-            this.firstName = FN;
-        }
-        public void setLN(string LN){
-            this.lastName = LN;
-        }
-        public void setEmail(string email){
-            this.emailAddress = email;
-        }
+
     }
 }
-
-
-
-
-
-   
-        
-      
-   
