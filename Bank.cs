350aab8ecf4f1d0a85642486dc1647ae27606b84
Joseph-Garzon <55984853+Joseph-Garzon@users.noreply.github.com>
2020-02-07 07:53:25 -0600

    Update Bank.cs

--- Bank.cs
+++ Bank.cs
@@ -1,29 +1,56 @@
 using System;
 namespace Banking
 {
-    
+
     public class Bank
     {
+        public String Name { get; set;}
         SavingsAccount savings;
         CheckingAccount checking;
         CD cD;
         MoneyMarketAccount moneyMarket;
         BrokerageAccount brokerage;
-        
 
-        public Bank(double savInt, double checkInt, double CDInt, double monMarkInt, double brokInt) {
-            savings = new SavingsAccount(0, savInt);
-            checking = new CheckingAccount(0, checkInt);
-            cD = new CD(0, CDInt);
-            moneyMarket = new MoneyMarketAccount(0, monMarkInt);
-            brokerage = new BrokerageAccount(0, brokInt);
+
+        public Bank(String Name, double savInt, double checkInt, double CDInt, double monMarkInt, double brokInt, int principle)
+        {
+            this.Name = Name;
+            savings = new SavingsAccount(principle, savInt);
+            checking = new CheckingAccount(principle, checkInt);
+            cD = new CD(principle, CDInt);
+            moneyMarket = new MoneyMarketAccount(principle, monMarkInt);
+            brokerage = new BrokerageAccount(principle, brokInt);
         }
 
+        public Accounts getAccountByType(int sel)
+        {
+            if (sel==1)
+            {
+                return savings;
+            }
+            else if (sel == 2)
+            {
+                return checking;
+            }
+            else if (sel == 4)
+            {
+                return cD;
+            }
+            else if (sel == 3)
+            {
+                return moneyMarket;
+            }
+            else if (sel == 5)
+            {
+                return brokerage;
+            }
+            else
+            {
+                Console.Write("!");
+                return checking;
+            }
 
-
-
+        }
 
     }
 }
-
-           
